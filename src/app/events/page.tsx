87cb0ff1960b24a40269c2ecf1c4f59a78ab69b1
Shinjan saha <shinjansaha02@gmail.com>
2024-11-23 12:23:24 +0530

    Added Google Analytics & events arranged

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -18,15 +18,50 @@ const fakeEvents = [
 
 // Past Events
 
-
 {
-  title: "Introduction to Version Control",
-  description: "irst Steps to Version Control*: Covering the basics and discussed its crucial role in software development. ",
-  badges: ["Git", "Github"],
-  timeAgo: "18/10/2024",
+  title: "Mastering Linux & Bash—Explore, Automate, Secure!",
+  description: "Dive into the world of Linux and Bash to transform your workflows, automate tasks, and strengthen security like never before! ",
+  badges: ["Linux", "Bash", "Automatation", "Security"],
+  timeAgo: "28/10/2024",
   status: "past",
-  seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FVersion%20Control%20with%20Git",
-  image :"https://i.postimg.cc/vHkB0f3r/1729260354253.jpg",
+  seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FMastering%20Linux%20and%20Bash%20Scripting",
+  image:"https://i.postimg.cc/nrBYSpqZ/Whats-App-Image-2024-10-28-at-8-09-14-PM.jpg",
+},
+{
+  title: "Next Level Web Dev—Mastering Next.js!",
+  description: "Get ready to supercharge your web development skills with the power of Next.js! ",
+  badges: ["NextJS", "SSR","CSR","TailwindCSS"],
+  timeAgo: "27/10/2024",
+  status: "past",
+  seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FNext%20Level%20Web%20Dev%2D%20Mastering%20Next%2EJs",
+  image:"https://i.postimg.cc/hj91pKLn/Whats-App-Image-2024-10-27-at-8-07-54-PM.jpg",
+},
+{
+  title: "React Revolution is ON! ",
+  description: "Get ready to master React.js and Tailwind CSS in today’s session! ",
+  badges: ["ReactJS", "TailwindCSS"],
+  timeAgo: "26/10/2024",
+  status: "past",
+  seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FReact%20Revolution%20%2D%20Building%20React%20Js%20and%20Tailwind%20CSS%20Applications", 
+  image:"https://i.postimg.cc/mgG6s6r8/Whats-App-Image-2024-10-26-at-8-10-47-PM.jpg",
+},
+{
+  title: "Dive into PHP & MySQL Essentials!",
+  description: "Get ready to level up your skills and make your mark in web dev! Don’t miss it!",
+  badges: ["PHP", "MYSQL"],
+  timeAgo: "25/10/2024",
+  status: "past",
+  seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FWeb%20Dev%20101%20%2D%20Introduction%20to%20PHP%20%26%20MySQL",
+  image :"https://i.postimg.cc/yxYpRq0P/Whats-App-Image-2024-10-25-at-8-08-41-PM.jpg",
+},
+{
+  title: "Building Minimal API with .NET",
+  description: "Ready to unlock the secrets of  API Wizardry? Join us for a session to explore how API can revolutionize your approach to tech !!",
+  badges:[".NET", "API", "Objective C"],
+  timeAgo: "20/10/2024",
+  status: "past",
+  seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FBuilding%20Minimal%20API%20with%20%2ENET",
+  image :"https://i.postimg.cc/N0BMmbPM/Whats-App-Image-2024-10-20-at-8-23-07-PM.jpg",
 },
 {
   title: " Level up with Data! Don’t Miss the HackNexus Data Science Webinar",
@@ -37,52 +72,22 @@ const fakeEvents = [
   seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FData%20Science%20Demystified",
   image: "https://i.postimg.cc/2yHYLRXd/19th-DS.png",
 },
-  {
-    title: "Building Minimal API with .NET",
-    description: "Ready to unlock the secrets of  API Wizardry? Join us for a session to explore how API can revolutionize your approach to tech !!",
-    badges:[".NET", "API", "Objective C"],
-    timeAgo: "20/10/2024",
-    status: "past",
-    seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FBuilding%20Minimal%20API%20with%20%2ENET",
-    image :"https://i.postimg.cc/N0BMmbPM/Whats-App-Image-2024-10-20-at-8-23-07-PM.jpg",
-  },
+{
+  title: "Introduction to Version Control",
+  description: "irst Steps to Version Control*: Covering the basics and discussed its crucial role in software development. ",
+  badges: ["Git", "Github"],
+  timeAgo: "18/10/2024",
+  status: "past",
+  seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FVersion%20Control%20with%20Git",
+  image :"https://i.postimg.cc/vHkB0f3r/1729260354253.jpg",
+},
+
   
-  {
-    title: "Dive into PHP & MySQL Essentials!",
-    description: "Get ready to level up your skills and make your mark in web dev! Don’t miss it!",
-    badges: ["PHP", "MYSQL"],
-    timeAgo: "25/10/2024",
-    status: "past",
-    seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FWeb%20Dev%20101%20%2D%20Introduction%20to%20PHP%20%26%20MySQL",
-    image :"https://i.postimg.cc/yxYpRq0P/Whats-App-Image-2024-10-25-at-8-08-41-PM.jpg",
-  },
-  {
-    title: "React Revolution is ON! ",
-    description: "Get ready to master React.js and Tailwind CSS in today’s session! ",
-    badges: ["ReactJS", "TailwindCSS"],
-    timeAgo: "26/10/2024",
-    status: "past",
-    seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FReact%20Revolution%20%2D%20Building%20React%20Js%20and%20Tailwind%20CSS%20Applications", 
-    image:"https://i.postimg.cc/mgG6s6r8/Whats-App-Image-2024-10-26-at-8-10-47-PM.jpg",
-  },
-  {
-    title: "Next Level Web Dev—Mastering Next.js!",
-    description: "Get ready to supercharge your web development skills with the power of Next.js! ",
-    badges: ["NextJS", "SSR","CSR","TailwindCSS"],
-    timeAgo: "27/10/2024",
-    status: "past",
-    seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FNext%20Level%20Web%20Dev%2D%20Mastering%20Next%2EJs",
-    image:"https://i.postimg.cc/hj91pKLn/Whats-App-Image-2024-10-27-at-8-07-54-PM.jpg",
-  },
-  {
-    title: "Mastering Linux & Bash—Explore, Automate, Secure!",
-    description: "Dive into the world of Linux and Bash to transform your workflows, automate tasks, and strengthen security like never before! ",
-    badges: ["Linux", "Bash", "Automatation", "Security"],
-    timeAgo: "28/10/2024",
-    status: "past",
-    seeMoreLink: "https://stdntpartners-my.sharepoint.com/personal/aditya_seth_studentambassadors_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Faditya%5Fseth%5Fstudentambassadors%5Fcom%2FDocuments%2FHackNexus%2FMastering%20Linux%20and%20Bash%20Scripting",
-    image:"https://i.postimg.cc/nrBYSpqZ/Whats-App-Image-2024-10-28-at-8-09-14-PM.jpg",
-  },
+  
+
+  
+  
+  
   
 
 ];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/Footer";
+import Script from "next/script";
 
 export const metadata = {
   title: "Microsoft LSA Nexus Chapter",
@@ -15,7 +16,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body className="bg-black text-white">
+        {/* Google Analytics Script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D1HYC0GL28"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-D1HYC0GL28');
+          `}
+        </Script>
+
+        
         <Navbar />
         {children}
         <Footer />
